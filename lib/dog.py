f0b770bfbe6d31e5a0352643aac463b6ab24ed10
Shanley Caswell <shanleycaswell@gmail.com>
2023-07-16 21:57:17 -0400

    tests passing with some version issues

--- lib/dog.py
+++ lib/dog.py
@@ -1,22 +1,36 @@
-from models import Dog
+from models import Dog, Base
+from sqlalchemy import create_engine, and_
 
-def create_table(base):
-    pass
+from sqlalchemy.orm import sessionmaker
+
+
+def create_table(Base, engine):
+    
+    engine = create_engine('sqlite:///dogs.db')
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    
 
 def save(session, dog):
-    pass
+    session.bulk_save_objects([dog])
+    session.commit()
 
 def get_all(session):
-    pass
+    all_dogs = session.query(Dog).all()
+    return all_dogs
 
 def find_by_name(session, name):
-    pass
+    query = session.query(Dog).filter(Dog.name == name).first()
+    return query
 
 def find_by_id(session, id):
-    pass
+    query = session.query(Dog).filter(Dog.id == id).first()
+    return query
 
 def find_by_name_and_breed(session, name, breed):
-    pass
+    query = session.query(Dog).filter(and_(Dog.name == name, Dog.breed == breed)).first()
+    return query
 
 def update_breed(session, dog, breed):
-    pass
+    session.query(Dog).update({Dog.breed: breed})
